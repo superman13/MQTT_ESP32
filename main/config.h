420cbf86930fec344813ded6f5374df7ee111681
Maimescu Ştefan <s.maimescu@gmail.com>
2018-05-18 19:54:15 +0300

    new

---
+++ main/config.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define CONFIG_WIFI_SSID "Arobs Software"
+#define CONFIG_WIFI_PASSWORD "arobs_wifi"
+
+#endif // !CONFIG_H
--- main/main.c
+++ main/main.c
@@ -7,21 +7,186 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <stdint.h>
+#include <stddef.h>
+#include "esp_wifi.h"
 #include "esp_system.h"
 #include "nvs_flash.h"
 
+#include "adc.h"
+
+#include "esp_event_loop.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+#include "config.h"
+
+static const char *TAG = "MQTT_SAMPLE";
+
+static EventGroupHandle_t wifi_event_group;
+const static int CONNECTED_BIT = BIT0;
+
+uint16_t adc_val;
+esp_mqtt_client_handle_t client;
+TaskHandle_t xHandle = NULL;
+
+void publish_data_task(void *pvParameter)
+{
+	char temp_string[100];
+	printf("Start Publish_data_task\n");
+	//sprintf(temp_string, "TEST");
+	adc1_config_width(ADC_WIDTH_12Bit);
+	adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_11db);
+	// send data every 10 seconds
+	while(1) {
+		adc_val = adc1_get_voltage(ADC1_CHANNEL_6);
+		sprintf(temp_string, "%4d", adc_val);
+		esp_mqtt_client_publish(client, "/soil_moisture/humidity", temp_string, strlen(temp_string), 0, 0);
+		vTaskDelay(10000 / portTICK_RATE_MS);
+	}
+	printf("Restarting now.\n");
+	fflush(stdout);
+	esp_restart();
+}
+
+static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
+{
+	client = event->client;
+	int msg_id;
+	// your_context_t *context = event->context;
+	switch(event->event_id) {
+	case MQTT_EVENT_CONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+		//msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+		msg_id = esp_mqtt_client_subscribe(client, "/soil_moisture/humidity", 0);
+		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+		break;
+	case MQTT_EVENT_DISCONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+		if (xHandle != NULL)
+		{
+			vTaskDelete(xHandle);
+		}
+		break;
+
+	case MQTT_EVENT_SUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+		xTaskCreate(&publish_data_task, "Publish Sensor Data", 2048, NULL, 5, xHandle);
+		//msg_id = esp_mqtt_client_publish(client, "/soil_moisture/humidity", "0", 0, 0, 0);
+		ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+		break;
+	case MQTT_EVENT_UNSUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+		break;
+	case MQTT_EVENT_PUBLISHED:
+		ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+		break;
+	case MQTT_EVENT_DATA:
+		ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+		//printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+		//printf("DATA=%.*s\r\n", event->data_len, event->data);
+		break;
+	case MQTT_EVENT_ERROR:
+		ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+		break;
+	}
+	return ESP_OK;
+}
+
+
+static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
+{
+	switch (event->event_id) {
+	case SYSTEM_EVENT_STA_START:
+		esp_wifi_connect();
+		break;
+	case SYSTEM_EVENT_STA_GOT_IP:
+		xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+
+		break;
+	case SYSTEM_EVENT_STA_DISCONNECTED:
+		esp_wifi_connect();
+		xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+		break;
+	default:
+		break;
+	}
+	return ESP_OK;
+}
+
+static void wifi_init(void)
+{
+	tcpip_adapter_init();
+	wifi_event_group = xEventGroupCreate();
+	ESP_ERROR_CHECK(esp_event_loop_init(wifi_event_handler, NULL));
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+	wifi_config_t wifi_config = {
+		.sta = {
+		.ssid = CONFIG_WIFI_SSID,
+		.password = CONFIG_WIFI_PASSWORD,
+	},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+	ESP_LOGI(TAG, "start the WIFI SSID:[%s] password:[%s]", CONFIG_WIFI_SSID, "******");
+	ESP_ERROR_CHECK(esp_wifi_start());
+	ESP_LOGI(TAG, "Waiting for wifi");
+	xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+}
+
+static void mqtt_app_start(void)
+{
+	const esp_mqtt_client_config_t mqtt_cfg = {
+		//.host = "192.168.82.70",
+		.host = "91.239.9.10",
+		.port = 1883,
+		.client_id = "ESP32_IoT_2",
+		.username = "test",
+		.password = "test",
+		.event_handle = mqtt_event_handler,
+	};
+
+	esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+	esp_mqtt_client_start(client);
+}
+
+
 void hello_task(void *pvParameter)
 {
-    printf("Hello world!\n");
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-    }
+	
+	char temp_string[100];
+	printf(" connected!\n");
+	//sprintf(temp_string, "TEST");
+	adc1_config_width(ADC_WIDTH_12Bit);
+	adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_11db);
+	// send data every 5 seconds
+	while(1) {
+		//printf("Publishing message\n");
+		adc_val = adc1_get_voltage(ADC1_CHANNEL_6);
+		
+		sprintf(temp_string, "%4d", adc_val);
+		//mqtt_publish(client, "/soil_moisture/humidity", temp_string, strlen(temp_string), 0, 0);
+		//esp_mqtt_client_publish(client, "/topic/qos0", 0);
+		//sprintf(temp_string, "%4d", nr);
+		//mqtt_publish(client, "/esp_32/msg_nr", temp_string, strlen(temp_string), 1, 0);
+		vTaskDelay(10000 / portTICK_RATE_MS);
+	}
     printf("Restarting now.\n");
     fflush(stdout);
-    system_restart();
+    esp_restart();
 }
 
 #ifdef __cplusplus
@@ -29,6 +194,19 @@ extern "C"
 #endif
 void app_main()
 {
-    nvs_flash_init();
-    xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
+	ESP_LOGI(TAG, "[APP] Startup..");
+	ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+	ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+	esp_log_level_set("*", ESP_LOG_INFO);
+	esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+	esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+	nvs_flash_init();
+	wifi_init();
+	mqtt_app_start();
+    //xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
 }
